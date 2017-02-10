2e9ab92ae24b63a6e8085f31831711246b83bde1
Michał Krzyżanowski <michal.krzyzanowski+github@gmail.com>
2017-02-10 13:38:06 +0100

    add ability to set nil values on rates and files
    it gives ability to intantiate Label with “failed” status

--- lib/postmen/label.rb
+++ lib/postmen/label.rb
@@ -3,8 +3,8 @@ class Postmen
     attribute :id, Types::UUID
     attribute :status, Types::String
     attribute :tracking_numbers, Types::Array.member(Types::String)
-    attribute :files, Types::Array.member(Types::Hash)
-    attribute :rate, Types::Hash
+    attribute :files, Types::Strict::Nil | Types::Array.member(Types::Hash)
+    attribute :rate, Types::Strict::Nil | Types::Hash
 
     def self.all(options = {})
       LabelCollection.all(options)
