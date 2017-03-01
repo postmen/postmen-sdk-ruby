cb66afddcddb9c1574b0b671535aad510ee89020
Michał Krzyżanowski <michal.krzyzanowski+github@gmail.com>
2017-03-01 15:44:37 +0100

    Move weight definition

--- lib/postmen/parcel.rb
+++ lib/postmen/parcel.rb
@@ -5,8 +5,8 @@ class Postmen
   class Parcel < Dry::Struct
     attribute :description, Types::String
     attribute :box_type, Types::String
+    attribute :weight, Types::Weight
     attribute :dimension, Types::Dimension
     attribute :items, Types::Array.member(Types::Item)
-    attribute :weight, Types::Weight
   end
 end
